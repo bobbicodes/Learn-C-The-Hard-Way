9b0bdacc0635928d6f7c9784639839f3ba69c2d8
Bobby Towers <sdfwer124@gmail.com>
2018-03-27 17:17:17 -0700

    compiles clean, all 6 unit tests passed

--- liblcthw/tests/runtests.sh
+++ liblcthw/tests/runtests.sh
@@ -1,19 +1,19 @@
-echo "Running unit tests:"
+#!/bin/bash
 
-for i in tests/*_tests
+echo "=========== Running unit tests ============="
+
+for i in tests/*_tests;
 do
-	if test -f $i
-	then
-		if $VALGRIND ./$i 2>> tests/tests.log
-		then
-			echo $i PASS
-		else
-			echo "ERROR in test $i: here's tests/tests.log"
-			echo "------"
-			tail tests/tests.log
-			exit 1
-		fi
-	fi
+    if [ -f $i ]; then
+        if $VALGRIND ./$i 2>> tests/tests.log; then
+            echo $i PASS
+        else
+            echo "ERROR in tests $i: here's tests/tests.log"
+            echo "------"
+            tail tests/tests.log
+            exit 1
+        fi
+    fi
 done
 
-echo ""
+echo "=========== Unit tests Done  ============="
